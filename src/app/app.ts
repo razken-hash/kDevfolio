a2c4814bafb024b05aaae53bfd4cf96ab582d040
AbdErrazak KENNICHE <ka_kenniche@esi.dz>
2025-11-19 11:25:56 +0100

    feat: add footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Home } from './home/home';
 import { Contact } from './contact/contact';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Header, Home, Contact
+    Header, Home, Contact, Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
